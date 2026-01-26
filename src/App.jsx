cb298b2693321157e5c2253f3ec8eaf7a75a2417
RIKESHWARAN M <rikeshrikey@gmail.com>
2026-01-26 10:39:56 +0530

    removed home

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,7 @@ import AppRoutes from "./routes/AppRoutes";
 import "./assets/styles/global.css";
 
 function App() {
-  return (
-    <>
-    
-      <div className="glow-orb orb1"></div>
-      <div className="glow-orb orb2"></div>
-      <div className="glow-orb orb3"></div>
-    <AppRoutes />
-    </>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
